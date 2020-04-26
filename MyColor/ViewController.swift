9ce89bf0233cf96e4836c837b5d4418e0f01dce0
samsadch <samsadch@gmail.com>
2020-04-26 23:13:02 +0400

    UI Button connected and background changed

--- MyColor/ViewController.swift
+++ MyColor/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var isPurple = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func changeColor(_ sender: Any) {
+        if(isPurple){
+            view.backgroundColor = UIColor.red
+        }else{
+            view.backgroundColor = UIColor.purple
+        }
+        isPurple = !isPurple
+    }
+    
 }
 
